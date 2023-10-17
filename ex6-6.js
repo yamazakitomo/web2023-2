6333146336f7411f68a36f6bf8baf2dcd2b5817b
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-10-17 10:33:27 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,4 +1,4 @@
 var who66='world';
 function sayhello66(){alert('Hello,'+who66+'!');}
-function taro66(){alert('太郎が来た')who66='太郎';}
-function hanako66(){alert('花子が来た')who66='花子';}
+function taro66(){who66='太郎';}
+function hanako66(){who66='花子';}
