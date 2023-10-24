ab711ca86ab94eb894006bc39ece7d03a7d245ca
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-10-24 10:36:57 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -1,3 +1,4 @@
 function sayhello68(){alert('Hello,'+document.getElementById('who68').innerHTML+'!');}
 function taro68(){document.ElementById('who68').innerHTML='太郎';}
 function hanako68(){document.getElementById('who68').innerHTML='花子';}
+function gazo688({document.getElementById('who688').innerHTML='<img src="./image/" alt="オン">';})
