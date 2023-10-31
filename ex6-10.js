6ea8b7b5b765a33838fa0a043de783acdced113e
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-10-31 13:36:14 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,4 @@
 function imgchange1(){document.getElementById('logo').src='./image/bluebird_baka.png';}
 function imgchange2(){document.getElementById('logo').src='./image/bird_aoitori_bluebird.png';}
+function imgchange3(){document.getElementById('logo2').src='./image/'}
+function imgchange4(){document.getElementById('logo2').src='./image/'}
