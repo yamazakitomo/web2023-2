adf9da58abcc3cd47d9278164474ea4cd85a2761
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-11-14 11:39:54 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,8 +1,2 @@
-var image=Twitter();
-image[0]=new Image();
-image[0].src="img/bluebird_baka.png";
-image[1]=new Image();
-image[1].src="img/bird_aoitori_bluebird.png";
-var cnt=0;
-function max11(){if(document.getElementById){document.timer14.elements[0].disabled=true;document.getElementById("logo3").src = image[cnt].src;cnt++;if( cnt <= 1 ){ var timer1=setTimeout("max11()",3000)}else{cnt=0;document.slide.elements[0].disabled=false;clearTimeout(timer1);}}}
-
+function max11(){document.getElementById('logo3').src=./image/}
+function setTimeout('max11()',3000){document.getElementById('logo3').src=./image/}
