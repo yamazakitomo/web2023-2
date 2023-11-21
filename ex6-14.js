78decebdd4d0bae54ba996f5b8f316c2eb4ffa8a
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-11-21 11:07:25 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,3 +1,2 @@
 function imagechange5(){document.getElementById('logo3').src='./image/bluebird_baka.png';}
 function imagechange6(){document.getElementById('logo3').src='./image/bird_aoitori_bluebird.png';}
-//function setTimeout('max11()',3000){document.getElementById('logo3').src=./image/bird_aoitori_bluebird.png;}
