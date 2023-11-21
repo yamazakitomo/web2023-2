ccb2600dedc3a2d2c6d7e6fba67b2f9810ac1c08
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-11-21 10:26:44 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,3 @@
-function imagechange5(){document.getElementById('logo3').src='./image/bluebird_baka.png';}
+function imagechange5(){{document.getElementById('logo3').src='./image/bluebird_baka.png';}setTimeout("imagechange6()",3000;)}
+function imagechange6(){document.getElementById().src='./image/bird_aoitori_bluebird.png';}
 //function setTimeout('max11()',3000){document.getElementById('logo3').src=./image/bird_aoitori_bluebird.png;}
