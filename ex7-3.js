4e1441d91270d27d0114fbd72b57e45ade8877e2
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-11-28 10:16:03 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,1 +1,1 @@
-function add(){ var x =document.getElementById('input3').value;var y =document.getElementById('input4').value;alert(Number('x')/(('y'^2));}
+function add(){ var x =document.getElementById('input3').value;var y =document.getElementById('input4').value;alert(Number('x')/Number('y')^2);}
