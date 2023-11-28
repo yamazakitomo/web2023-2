0877e85a7dcddf9ad438dc6ff429e6c58b8e79db
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-11-28 10:59:24 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,1 +1,1 @@
-function add(){ var x =Number(document.getElementById('input3').value);var y =Number(document.getElementById('input4').value);alert(x/(y'^2));}
+function bmi(){ var x =Number(document.getElementById('input3').value);var y =Number(document.getElementById('input4').value);alert(x/(y'^2));}
