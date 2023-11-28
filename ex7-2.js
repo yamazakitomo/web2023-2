2c5dcbcf97ab3182892e0a1e8cd065629d2ccce6
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-11-28 10:32:50 +0900

    Update ex7-2.js

--- ex7-2.js
+++ ex7-2.js
@@ -1,1 +1,1 @@
-function add(){var x =document.getElementById('input1').value;var y =document.getElementById('input2').value;alert(Number(x)+Number(y));}
+function add(){var x =document.getElementById('input1').value;var y =document.getElementById('input2').value;alert(x+y);}
