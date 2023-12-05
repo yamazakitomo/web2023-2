3ddb2b90173591dd0fa57d18256b4870d92777a2
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-12-05 10:26:21 +0900

    Update ex7-9.js

--- ex7-9.js
+++ ex7-9.js
@@ -1,1 +1,1 @@
-function nazo3()if(document.getElementById('answer2').value='夢'){alert('あたり！');}else{alert('はずれ');}if(document.getElementById('answer2').value='ゆめ'){alert('あたり！');}else{alert('はずれ！');}
+function nazo3()if(document.getElementById('answer3').value='夢'){alert('あたり！');}else{alert('はずれ');}if(document.getElementById('answer3').value='ゆめ'){alert('あたり！');}else{alert('はずれ！');}
