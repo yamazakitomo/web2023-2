089898855388cd420a02fae6dcc661713bfe0ec2
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-12-05 10:02:12 +0900

    Update ex7-4.js

--- ex7-4.js
+++ ex7-4.js
@@ -1,1 +1,1 @@
-function nazo(){if(document.getElementById('answer').value=='夢'){alert('あたり！');}else{alert('はずれ');}}
+function nazo(){if(document.getElementById('answer').value!=='夢'){alert('あたり！');}else{alert('はずれ');}}
