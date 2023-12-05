b23b364ef8d898c147d38f6d8f91cd2b76cd6eca
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-12-05 10:17:27 +0900

    Create ex7-9.js

---
+++ ex7-9.js
@@ -0,0 +1,1 @@
+function nazo2()if(document.getElementById('answer2').value='夢'){alert('あたり！');}else{alert('はずれ');}if(document.getElementById('answer2').value='ゆめ'){alert('あたり！');}else{alert('はずれ！');}
