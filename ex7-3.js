cf83dcd100f4efd423969202b9da462ff87eb9df
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-12-12 09:54:03 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,3 +1,3 @@
-function bmi(){ var x =Number(document.getElementById('input3').value);var y =Number(document.getElementById('input4').value);alert(x/(y*y));}
+function bmi(){ var x =Number(document.getElementById('input3').value);var y =Number(document.getElementById('input4').value);}if{{(x/(y*y))<=18.5{alert('低体重');}}else if{18.5<=(x/(y*y))<=25{alert('普通');}}else if{(x/(y*y))<=25{alert('肥満');}}}
 function nazo3(){if(document.getElementById('answer3').value=='夢'){alert('あたり！');}else if(document.getElementById('answer3').value=='ゆめ'){alert('あたり！');}else{alert('はずれ！');}}
 (x+y)<=18.5 低体重 18.5<=(x+y)<=25 普通 (x+y)<=25 肥満
