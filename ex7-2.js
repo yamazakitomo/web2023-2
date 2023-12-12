673c7b208629e033b21e3dc7d5d655348e7a0fa7
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-12-12 09:32:16 +0900

    Update ex7-2.js

--- ex7-2.js
+++ ex7-2.js
@@ -1,1 +1,3 @@
 function add(){var x =Number(document.getElementById('input1').value);var y =Number(document.getElementById('input2').value);alert(x+y);}
+function nazo3(){if(document.getElementById('answer3').value=='夢'){alert('あたり！');}else if(document.getElementById('answer3').value=='ゆめ'){alert('あたり！');}else{alert('はずれ！');}}
+(x+y)<=18.5 低体重 18.5<=(x+y)<=25 普通 (x+y)<=25 肥満
