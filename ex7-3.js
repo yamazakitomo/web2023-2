a786c4f0a61fca7e2117973f133dbad30cf3db90
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-12-12 10:25:24 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,1 +1,1 @@
-function bmi(){ var x =Number(document.getElementById('input3').value);var y =Number(document.getElementById('input4').value);}if{((x/(y*y))<=18.5){alert('低体重');}else if(18.5<=(x/(y*y))<=25){alert('普通');}else if((x/(y*y))<=25){alert('肥満');}}
+function bmi(){ var x =Number(document.getElementById('input3').value);var y =Number(document.getElementById('input4').value);}if{((x/(y*y))<=18.5){alert('低体重');}else if(18.5<=(x/(y*y))<=25){alert('普通');}else{alert('肥満');}}
