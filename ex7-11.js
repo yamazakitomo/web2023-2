5bca3161a8b5c71cef06ab24b7bcc54a09f5cd19
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-12-15 14:07:57 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,1 +1,1 @@
-function uru(){ var X =Number(document.getElementById('seireki').value);if((X % 400==0) && (X % 100!=0) && (X % 4==0)){alert('西暦'+X+'年は閏年');}else{alert('西暦'+X+'年は平年');}}
+function uru(){ var X =Number(document.getElementById('seireki').value);if((X % 4==0 && X % 100!=0) || (X % 400==0)){alert('西暦'+X+'年は閏年');}else{alert('西暦'+X+'年は平年');}}
