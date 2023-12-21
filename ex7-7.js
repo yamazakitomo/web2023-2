db3cea1cb3a0e5819e07c96cf0a5fc4a3f48cca9
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-12-21 11:54:02 +0900

    Update ex7-7.js

--- ex7-7.js
+++ ex7-7.js
@@ -1,1 +1,1 @@
-function nazo77(){if(document.getElementById('answer77').value=='夢'){alert('あたり！');}else{alert('はずれ！');}}
+function nazo77(){if(document.getElementById('answer77').value == '夢'){alert('あたり！');}else{alert('はずれ！');}}
