56435858b4bb6af17ddb0084466f1f27fdf3589d
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-12-21 10:51:59 +0900

    Create ex7-10.js

---
+++ ex7-10.js
@@ -0,0 +1,1 @@
+function BMI(){ var x =Number(document.getElementById('weight').value);var y =Number(document.getElementById('feet').value);var z =Number(x/(y*y));if(z<=18.5){alert(z+'は低体重');}else if(z<25){alert(z+'は普通');}else{alert(z+'は肥満');}}
