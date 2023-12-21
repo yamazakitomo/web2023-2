05328e84238db6ed201887cb4c9ae50d213b2307
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-12-21 11:04:39 +0900

    Update ex7-6.js

--- ex7-6.js
+++ ex7-6.js
@@ -1,1 +1,1 @@
-function nazo2(){if(document.getElementById('answer2').value=='夢'){alert('あたり！');}else{alert('はずれ');}}
+function nazo2(){if(document.getElementById('answer2').value=='夢'){alert('はずれ');}else{alert('あたり！');}}
